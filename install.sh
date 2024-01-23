bfbd9bfa47cce226fe257b501940e5e41a624c2c
Kashyap Sindhav <121540898+Kashyap682@users.noreply.github.com>
2024-01-23 21:38:57 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,29 @@
+!# /usr/bin/bash
+
+sudo dnf update
+sudo dnf install xorg-x11-server-Xorg xorg-x11-xinit
+sudo dnf git nano python pip
+sudo dnf install bspwm sxhkd polybar picom dmenu rofi
+sudo dnf install nitrogen thunar firefox geany
+
+mkdir .config
+mkdir polybar
+
+git clone --depth=1 https://github.com/adi1090x/polybar-themes.git
+cd polybar-themes
+chmod +x setup.sh
+./setup.sh
+
+rm -R polybar
+
+git clone https://github.com/Kashyap682/mydots.git
+cd mydots
+cp -R mydots/config/* .config/
+
+chmod +x bspwm/bspwmrc
+chmod +x sxhkd/sxhkdrc
+
+echo "exec bspwm" > .xinitrc
+
+
+
