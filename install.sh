f7e197ccaff83fbbe3ee94f2d6e86861bbc300b1
Kashyap Sindhav <121540898+Kashyap682@users.noreply.github.com>
2024-01-23 22:26:13 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,20 +6,19 @@ sudo dnf install bspwm sxhkd polybar picom dmenu rofi
 sudo dnf install nitrogen thunar firefox geany
 
 mkdir .config
-mkdir polybar
 
 git clone --depth=1 https://github.com/adi1090x/polybar-themes.git
 cd polybar-themes
 chmod +x setup.sh
 ./setup.sh
 
-rm -R polybar
+rm -R .config/polybar
 
 cd mydots
-cp -R mydots/config/* .config/
+mv -R mydots/config/* .config/
 
-chmod +x bspwm/bspwmrc
-chmod +x sxhkd/sxhkdrc
+chmod +x .config/bspwm/bspwmrc
+chmod +x .config/sxhkd/sxhkdrc
 
 echo "exec bspwm" > .xinitrc
 
