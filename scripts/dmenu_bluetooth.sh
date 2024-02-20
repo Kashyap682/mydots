bfab927a410cb3a25cea5d55d6d3a25050563669
Kashyap Sindhav <121540898+Kashyap682@users.noreply.github.com>
2024-02-20 23:21:51 +0530

    Update dmenu_bluetooth.sh

--- scripts/dmenu_bluetooth.sh
+++ scripts/dmenu_bluetooth.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 . "${HOME}/.cache/wal/colors.sh"
 
-devices=$(bluetoothctl devices | grep Device | cut -d ' ' -f 2- | sed 's/Device //' | dmenu -c -l 4 -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -fn "Fira Code:size=12")
+action=$(echo -e "Toggle Bluetooth\nScan for Devices\nConnect to Device" | dmenu -l 3 -p "Bluetooth Menu:" -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -fn "Fira Code:size=12")
 
-if [ -n "$devices" ]; then
-    bluetoothctl connect "$devices"
-fi
-
+case "$action" in
+    "Toggle Bluetooth")
+        bluetoothctl power off
+        sleep 1
+        bluetoothctl power on
+        ;;
+    "Scan for Devices")
+        bluetoothctl scan on &
+        ;;
+    "Connect to Device")
+        devices=$(bluetoothctl devices | grep Device | cut -d ' ' -f 2- | sed 's/Device //' | dmenu -l 10 -p "Select Bluetooth device:" -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -fn "Fira Code:size=12")
+        if [ -n "$devices" ]; then
+            bluetoothctl connect "$devices"
+        fi
+        ;;
+esac
